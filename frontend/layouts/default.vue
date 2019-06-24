57b0dd637195b0b47ed2139aa720ce1cb1d8878e
Dawid Kręcisz <dawid.krecisz@lemisoft.pl>
2019-06-24 22:09:29 +0200

    Change docker place

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,94 +1,105 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
+  <div>
+    <v-app
+      style="background-color: black;"
+      v-if="!ready"
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
+      <img
+        src="~/assets/file.jpg"
+        style="max-height: 100%; max-width: 100%; margin: 200px 0">
+      <div style="background-color: black">..</div>
+    </v-app>
+    <v-app v-if="ready">
+      <v-navigation-drawer
+        :clipped="clipped"
+        :mini-variant="miniVariant"
+        app
+        fixed
+        v-model="drawer"
+      >
+        <v-list>
+          <v-list-tile
+            :key="i"
+            :to="item.to"
+            exact
+            router
+            v-for="(item, i) in items"
+          >
+            <v-list-tile-action>
+              <v-icon v-html="item.icon"/>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"/>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar
+        :clipped-left="clipped"
+        app
+        fixed
+      >
+        <v-toolbar-side-icon @click="drawer = !drawer"/>
+        <v-btn
+          @click.stop="miniVariant = !miniVariant"
+          icon
         >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer"/>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+          <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"/>
+        </v-btn>
+        <v-btn
+          @click.stop="clipped = !clipped"
+          icon
+        >
+          <v-icon>web</v-icon>
+        </v-btn>
+        <v-btn
+          @click.stop="fixed = !fixed"
+          icon
+        >
+          <v-icon>remove</v-icon>
+        </v-btn>
+        <v-toolbar-title v-text="title"/>
+        <v-btn
+          @click.stop="rightDrawer = !rightDrawer"
+          icon
+        >
+          <v-icon>menu</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-content>
+        <v-container>
+          <nuxt/>
+        </v-container>
+      </v-content>
+      <v-navigation-drawer
+        :right="right"
+        fixed
+        temporary
+        v-model="rightDrawer"
       >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"/>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
+        <v-list>
+          <v-list-tile @click.native="right = !right">
+            <v-list-tile-action>
+              <v-icon light>compare_arrows</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-footer
+        :fixed="fixed"
+        app
       >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt/>
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+        <span>&copy; 2019</span>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
+      ready: false,
       clipped: false,
       drawer: true,
       fixed: false,
