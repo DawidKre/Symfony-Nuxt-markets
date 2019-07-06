132bcf44a5240487b6f8f3415c9b22ef07679098
Dawid Kręcisz <dawid.krecisz@lemisoft.pl>
2019-07-06 15:21:09 +0200

    Quasar implementation

---
+++ frontend/src/layouts/MyLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="hHh lpR fff">
+    <q-header class="bg-primary text-white" elevated>
+      <q-toolbar>
+        <q-btn @click="left = !left" dense flat icon="menu" round/>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"/>
+          </q-avatar>
+          Markets App {{ test() }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      :breakpoint="500"
+      :width="200"
+      bordered
+      content-class="bg-grey-3"
+      show-if-above
+      v-model="drawer"
+    >
+      <q-scroll-area class="fit">
+        <q-list :key="index" v-for="(menuItem, index) in menuList">
+          <q-item :active="menuItem.label === 'Outbox'" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon :name="menuItem.icon"/>
+            </q-item-section>
+            <q-item-section>
+              {{ menuItem.label }}
+            </q-item-section>
+          </q-item>
+
+          <q-separator v-if="menuItem.separator"/>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Inbox',
+    separator: true
+  },
+  {
+    icon: 'send',
+    label: 'Outbox',
+    separator: false
+  },
+  {
+    icon: 'delete',
+    label: 'Trash',
+    separator: false
+  },
+  {
+    icon: 'error',
+    label: 'Spam',
+    separator: true
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false
+  }
+]
+
+export default {
+  data () {
+    return {
+      drawer: true,
+      menuList
+    }
+  },
+  methods: {
+    test () {
+      return 123
+    }
+  }
+}
+</script>
