cbaf4effe64ea11205aa8a57a3aa8f9fd57e1b18
Dawid Kręcisz <dawid.krecisz@lemisoft.pl>
2019-08-06 22:38:50 +0200

    RYNKI-40 Set events in scrapers

--- backend/src/Controller/ScraperController.php
+++ backend/src/Controller/ScraperController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
-use App\BusinessLogic\Importer\Exception\ImporterException;
-use App\BusinessLogic\Importer\Service\ImportService;
-use App\Entity\ImporterLog;
-use DateTime;
+use App\BusinessLogic\Scraper\Event\MarketNotScrapedEvent;
+use App\BusinessLogic\Scraper\Event\MarketScrapedEvent;
+use App\BusinessLogic\Scraper\Event\MarketScrapingErrorEvent;
+use App\BusinessLogic\Scraper\Event\ScrapingProcessFinishedEvent;
+use App\BusinessLogic\Scraper\Event\ScrapingProcessStartedEvent;
+use App\BusinessLogic\Scraper\Exception\MarketNotScrapedException;
+use App\BusinessLogic\Scraper\Exception\ScraperException;
+use App\BusinessLogic\Scraper\Factory\ScrapeMarketFactory;
+use App\Entity\Market;
 use Doctrine\ORM\EntityManagerInterface;
+use League\Csv\CannotInsertRecord;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,20 +27,31 @@ class ScraperController extends AbstractController
     /**
      * @Route("/scraper", name="scraper")
      *
-     * @param ImportService          $importService
-     * @param EntityManagerInterface $entityManager
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param ScrapeMarketFactory      $scraperManager
+     * @param EntityManagerInterface   $entityManager
      *
      * @return JsonResponse
-     *
-     * @throws ImporterException
      */
-    public function index(ImportService $importService, EntityManagerInterface $entityManager): Response
+    public function index(EventDispatcherInterface $eventDispatcher, ScrapeMarketFactory $scraperManager, EntityManagerInterface $entityManager): Response
     {
-        $importStartDate = new DateTime();
-        $importerLogs = $entityManager->getRepository(ImporterLog::class)->getNotImported();
-        foreach ($importerLogs as $importerLog) {
-            $importService->import($importerLog, $importStartDate);
+        $eventDispatcher->dispatch(new ScrapingProcessStartedEvent());
+        $markets = $entityManager->getRepository(Market::class)->findAll();
+
+        foreach ($markets as $market) {
+            try {
+                $records = $scraperManager->createScraper($market)->scrapeMarket();
+                $eventDispatcher->dispatch(new MarketScrapedEvent($market, $records));
+            } catch (MarketNotScrapedException $e) {
+                $eventDispatcher->dispatch(new MarketNotScrapedEvent($market, $e->getMessage()));
+            } catch (ScraperException $e) {
+                $eventDispatcher->dispatch(new MarketScrapingErrorEvent($market, $e->getMessage()));
+            } catch (CannotInsertRecord $e) {
+                $eventDispatcher->dispatch(new MarketScrapingErrorEvent($market, $e->getMessage()));
+            }
         }
+
+        $eventDispatcher->dispatch(new ScrapingProcessFinishedEvent());
 
         return $this->render('scraper/index.html.twig');
     }
